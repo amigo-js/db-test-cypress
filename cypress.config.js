39092f223492494ec9158ebfed6f41f5ca4e6a97
amigo-js <120602850+amigo-js@users.noreply.github.com>
2023-05-12 00:18:42 +0200

    test

--- cypress.config.js
+++ cypress.config.js
@@ -1,14 +1,6 @@
-const { setupDb } = require('./setup-db');
+
 
 module.exports = (on, config) => {
-  // Настройка базы данных перед запуском тестов
-  on('task', {
-    setupDb: async () => {
-      await setupDb();
-      return null;
-    }
-  });
-
   // Очистка базы данных после завершения всех тестов
   on('task', {
     clearDb: async () => {
@@ -19,3 +11,27 @@ module.exports = (on, config) => {
 
   return config;
 };
+
+
+
+// const { setupDb } = require('./setup-db');
+
+// module.exports = (on, config) => {
+//   // Настройка базы данных перед запуском тестов
+//   on('task', {
+//     setupDb: async () => {
+//       await setupDb();
+//       return null;
+//     }
+//   });
+
+//   // Очистка базы данных после завершения всех тестов
+//   on('task', {
+//     clearDb: async () => {
+//       // Выполните здесь код для очистки базы данных
+//       return null;
+//     }
+//   });
+
+//   return config;
+// };
