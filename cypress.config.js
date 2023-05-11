f598f09277a2f57949a1e914cd92b1a5b5d0317b
amigo-js <120602850+amigo-js@users.noreply.github.com>
2023-05-12 00:53:56 +0200

    check

--- cypress.config.js
+++ cypress.config.js
@@ -1,3 +1,4 @@
+
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
@@ -7,43 +8,122 @@ module.exports = defineConfig({
       on('task', {
         clearData: async () => {
           // Выполните здесь код для очистки базы данных
+          await postgresClient.query('DELETE FROM employees;');
           return null;
         },
         connectToPostgres: async () => {
           // Выполните здесь код для подключения к PostgreSQL
+          // Пример:
+          const { Client } = require('pg');
+          const postgresClient = new Client({
+            host: 'localhost',
+            port: 5432,
+            user: 'your-username',
+            password: 'your-password',
+            database: 'your-database',
+          });
+          await postgresClient.connect();
           return null;
         },
-        createTable: async () => {
-          // Выполните здесь код для подключения к PostgreSQL
+        createTable: async ({ tableName }) => {
+          // Выполните здесь код для создания таблицы в PostgreSQL с указанным именем tableName
+          // Пример:
+          await postgresClient.query(`CREATE TABLE ${tableName} (id SERIAL PRIMARY KEY, name VARCHAR(255));`);
           return null;
         },
-        tableExists: async () => {
-          // Выполните здесь код для подключения к PostgreSQL
+        tableExists: async ({ tableName }) => {
+          // Выполните здесь код для проверки существования таблицы в PostgreSQL с указанным именем tableName
+          // Пример:
+          const result = await postgresClient.query(`SELECT EXISTS (SELECT FROM information_schema.tables WHERE table_name = '${tableName}');`);
+          const tableExists = result.rows[0].exists;
+          return tableExists;
+        },
+        addColumns: async ({ tableName, columns }) => {
+          // Выполните здесь код для добавления столбцов в таблицу в PostgreSQL
+          // Пример:
+          for (const column of columns) {
+            await postgresClient.query(`ALTER TABLE ${tableName} ADD COLUMN ${column} VARCHAR(255);`);
+          }
           return null;
         },
-        addColumns: async () => {
-          // Выполните здесь код для подключения к PostgreSQL
+        getColumns: async ({ tableName }) => {
+          // Выполните здесь код для получения столбцов таблицы в PostgreSQL
+          // Пример:
+          const result = await postgresClient.query(`SELECT column_name FROM information_schema.columns WHERE table_name = '${tableName}';`);
+          const columns = result.rows.map(row => row.column_name);
+          return columns;
+        },
+        insertData: async ({ tableName, data }) => {
+          // Выполните здесь код для вставки данных в таблицу в PostgreSQL
+          // Пример:
+          const columns = Object.keys(data).join(', ');
+          const values = Object.values(data).map(value => `'${value}'`).join(', ');
+          await postgresClient.query(`INSERT INTO ${tableName} (${columns}) VALUES (${values});`);
           return null;
         },
-        getColumns: async () => {
-          // Выполните здесь код для подключения к PostgreSQL
-          return null;
+        getData: async ({ tableName }) => {
+          // Выполните здесь код для получения данных из таблицы в PostgreSQL
+          // Пример:
+          const result = await postgresClient.query(`SELECT * FROM ${tableName};`);
+          const data = result.rows;
+          return data;
         },
-        insertData: async () => {
-          // Выполните здесь код для подключения к PostgreSQL
-          return null;
-        },
-        getData: async () => {
-          // Выполните здесь код для подключения к PostgreSQL
-          return null;
-        },
-
       });
 
       return config;
     }
   }
-})
+});
+
+
+
+
+// const { defineConfig } = require('cypress');
+
+// module.exports = defineConfig({
+//   e2e: {
+//     setupNodeEvents(on, config) {
+//       // Очистка базы данных после завершения всех тестов
+//       on('task', {
+//         clearData: async () => {
+//           // Выполните здесь код для очистки базы данных
+//           return null;
+//         },
+//         connectToPostgres: async () => {
+//           // Выполните здесь код для подключения к PostgreSQL
+//           return null;
+//         },
+//         createTable: async () => {
+//           // Выполните здесь код для подключения к PostgreSQL
+//           return null;
+//         },
+//         tableExists: async () => {
+//           // Выполните здесь код для подключения к PostgreSQL
+//           return null;
+//         },
+//         addColumns: async () => {
+//           // Выполните здесь код для подключения к PostgreSQL
+//           return null;
+//         },
+//         getColumns: async () => {
+//           // Выполните здесь код для подключения к PostgreSQL
+//           return null;
+//         },
+//         insertData: async () => {
+//           // Выполните здесь код для подключения к PostgreSQL
+//           return null;
+//         },
+//         getData: async () => {
+//           // Выполните здесь код для подключения к PostgreSQL
+//           return null;
+//         },
+
+//       });
+
+//       return config;
+//     }
+//   }
+// })
 
 
 
