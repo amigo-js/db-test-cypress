464029774a2613dbe3ff665a27bfa241cf08df91
amigo-js <120602850+amigo-js@users.noreply.github.com>
2023-05-12 00:28:53 +0200

    refactor

--- cypress.config.js
+++ cypress.config.js
@@ -1,4 +1,4 @@
-
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
@@ -12,7 +12,7 @@ module.exports = defineConfig({
       });
 
       return config;
-  }
+    }
   }
 })
 
